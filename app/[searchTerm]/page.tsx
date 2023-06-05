82a8c9d2309da8549b680d075decf246286178b7
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-06-05 10:39:29 +0000

    Initial Commit

---
+++ app/[searchTerm]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    params: {
+        searchTerm: string
+    }
+}
+
+export default function page({params: {searchTerm}}: Props) {
+  return (
+    <div>page</div>
+  )
+}
