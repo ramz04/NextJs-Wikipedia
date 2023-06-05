8f3ca9f35e16d6af682cb16dd511fc913677555e
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-06-05 10:51:42 +0000

    Second Commit

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import getWikiResults from '@/lib/getWikiResults'
 
 type Props = {
     params: {
@@ -6,8 +7,23 @@ type Props = {
     }
 }
 
-export default function page({params: {searchTerm}}: Props) {
-  return (
-    <div>page</div>
+export default async function page({params: {searchTerm}}: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
+  const data = await wikiData
+  const results: Result[] | undefined = data?.query?.pages
+
+  const content = (
+    <main className='bg-slate-200 mx-auto max-w-lg py-1 min-h-screen'>
+      {results 
+      ? Object.values(results).map((result, ind) => {
+        return <p key={ind}>{JSON.stringify(result)}</p>
+      })
+      : <h2 className='p-2 text-xl'>
+          {`${searchTerm} Not Found`}
+        </h2>
+      }
+
+    </main>
   )
+  return content
 }
