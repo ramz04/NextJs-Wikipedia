68d7b11064c7818b3ad516f6f831f76752c88ae7
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-06-05 11:16:07 +0000

    Third Commit

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import getWikiResults from '@/lib/getWikiResults'
+import Item from './components/item'
 
 type Props = {
     params: {
@@ -7,7 +8,24 @@ type Props = {
     }
 }
 
-export default async function page({params: {searchTerm}}: Props) {
+export async function generateMetadata({params: {searchTerm}}: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
+  const data = await wikiData
+  const displayTerm = searchTerm.replaceAll('%20', '')
+
+  if(!data?.query?.pages) {
+    return {
+      title: `${displayTerm} Not Found`
+    }
+  }
+
+  return {
+    title: displayTerm,
+    description: `Search results for ${displayTerm}`
+  }
+}
+
+export default async function SearchResults({params: {searchTerm}}: Props) {
   const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
   const data = await wikiData
   const results: Result[] | undefined = data?.query?.pages
@@ -16,7 +34,7 @@ export default async function page({params: {searchTerm}}: Props) {
     <main className='bg-slate-200 mx-auto max-w-lg py-1 min-h-screen'>
       {results 
       ? Object.values(results).map((result, ind) => {
-        return <p key={ind}>{JSON.stringify(result)}</p>
+        return <Item key={ind} result={result} />
       })
       : <h2 className='p-2 text-xl'>
           {`${searchTerm} Not Found`}
@@ -25,5 +43,5 @@ export default async function page({params: {searchTerm}}: Props) {
 
     </main>
   )
-  return content
+  return content 
 }
